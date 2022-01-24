b6785a6898ac7ba203b6afc8bfc68052cf51dcbb
asm0dey <pavel.finkelshtein@gmail.com>
2022-01-24 11:32:01 +0300

    Fix readme, remove one more obsolete test

--- src/tts.service.ts
+++ src/tts.service.ts
@@ -1,11 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { DashaAcc } from './dashaacc.service';
 import { tts } from '@dasha.ai/sdk';
 
 @Injectable()
 export class TtsService {
+  private readonly logger = new Logger(TtsService.name);
   constructor(private readonly dashaApp: DashaAcc) {}
   async textToSpeech(text: string): Promise<Uint8Array> {
+    if (process.env['DASHA_TEXT_MESSAGE']) {
+      this.logger.log(`This should be translated to speech:
+      ${text}`);
+    }
     return await tts.synthesize(
       process.env['DASHA_TEXT_MESSAGE'] ?? text,
       {
